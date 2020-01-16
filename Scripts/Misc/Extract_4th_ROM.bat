91410a03e5a72eadaf2ba43152a8c9dbe0ee4dae
luffitys <banackidaniel@gmail.com>
2020-01-16 22:29:47 +0100

    Update Port and Extract Scripts to use Variables

---
+++ Scripts/Misc/Extract_4th_ROM.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+set NUMBER=4th
+set EXTRACTOR=Tools\Extractor
+set APKTOOL=Tools\APKTool
+set ROM=ROMS\%NUMBER%_ROM
+set SYSTEM=%ROM%\system
+set VENDOR=%ROM%\vendor
+set FRAMEWORKS=%ROM%\Frameworks
+set APK=%ROM%
+set TEMP=Temp_%NUMBER%
+
+@ECHO ON
+
+
+	:: Extract .zip
+cd ..\..
+7z x *.zip -o%TEMP%\ system.new.dat.br system.transfer.list vendor.new.dat.br vendor.transfer.list
+
+	:: Convert .dat.br -> .dat
+%Extractor%\Brotli.exe --decompress --in %TEMP%\system.new.dat.br --out %TEMP%\system.new.dat
+%Extractor%\Brotli.exe --decompress --in %TEMP%\vendor.new.dat.br --out %TEMP%\vendor.new.dat
+
+	:: Convert .dat -> .img
+%Extractor%\sdat2Img.exe %TEMP%\system.transfer.list %TEMP%\system.new.dat %TEMP%\system.img
+%Extractor%\sdat2Img.exe %TEMP%\vendor.transfer.list %TEMP%\vendor.new.dat %TEMP%\vendor.img
+
+	:: Extract .img
+7z x -aos %TEMP%\system.img -o%SYSTEM%
+7z x -aos %TEMP%\vendor.img -o%VENDOR%
+
+	:: Cleanup
+rmdir /Q /S %TEMP%
+
+	:: Avoid closing of CMD to see potential issues
+pause
--- Scripts/Port.bat
+++ Scripts/Port.bat
@@ -1,56 +1,60 @@
+@ECHO OFF
+
+set EXTRACTOR=Tools\Extractor
+set SYSTEM=ROMS\REQUIRED_ROM\ROM\system
+set VENDOR=ROMS\REQUIRED_ROM\ROM\vendor
+set APKTOOL=Tools\APKTool
+set CLASSES=ROMS\REQUIRED_ROM\Required_Classes
+
+@ECHO ON
+
+
 	:: Extract .zip
 cd ..
 7z x *.zip -oTemp\ system.new.dat.br system.transfer.list vendor.new.dat.br vendor.transfer.list
 
 	:: Convert .dat.br -> .dat
-Tools\Extractor\Brotli.exe --decompress --in Temp\system.new.dat.br --out Temp\system.new.dat
-Tools\Extractor\Brotli.exe --decompress --in Temp\vendor.new.dat.br --out Temp\vendor.new.dat
+%Extractor%\Brotli.exe --decompress --in Temp\system.new.dat.br --out Temp\system.new.dat
+%Extractor%\Brotli.exe --decompress --in Temp\vendor.new.dat.br --out Temp\vendor.new.dat
 
 	:: Convert .dat -> .img
-Tools\Extractor\sdat2Img.exe Temp\system.transfer.list Temp\system.new.dat Temp\system.img
-Tools\Extractor\sdat2Img.exe Temp\vendor.transfer.list Temp\vendor.new.dat Temp\vendor.img
+%Extractor%\sdat2Img.exe Temp\system.transfer.list Temp\system.new.dat Temp\system.img
+%Extractor%\sdat2Img.exe Temp\vendor.transfer.list Temp\vendor.new.dat Temp\vendor.img
 
 	:: Extract .img
-7z x -aos Temp\system.img -oROMS\REQUIRED_ROM\ROM\system
-7z x -aos Temp\vendor.img -oROMS\REQUIRED_ROM\ROM\vendor
+7z x -aos Temp\system.img -o%SYSTEM%
+7z x -aos Temp\vendor.img -o%VENDOR%
 
-	:: Add Frameworks [Trial and Error]
-java -jar Tools\APKTool\apktool.jar if ROMS\REQUIRED_ROM\ROM\system\framework\framework-res.apk -p Tools\APKTool\Frameworks
-java -jar Tools\APKTool\apktool.jar if ROMS\REQUIRED_ROM\ROM\system\system\framework\framework-res.apk -p Tools\APKTool\Frameworks
+	:: Add Frameworks
+java -jar %APKTOOL%\apktool.jar if %SYSTEM%\system\framework\framework-res.apk -p Tools\APKTool\Frameworks
 
-java -jar Tools\APKTool\apktool.jar if ROMS\REQUIRED_ROM\ROM\system\app\miui\miui.apk -p Tools\APKTool\Frameworks
-java -jar Tools\APKTool\apktool.jar if ROMS\REQUIRED_ROM\ROM\system\system\app\miui\miui.apk -p Tools\APKTool\Frameworks
+java -jar %APKTOOL%\apktool.jar if %SYSTEM%\system\app\miui\miui.apk -p %APKTOOL%\Frameworks
 
 	:: Decompile MiuiCamera
-java -jar Tools\APKTool\apktool.jar d --no-debug-info --output ..\ANXCamera_APK ROMS\REQUIRED_ROM\ROM\system\priv-app\MiuiCamera\MiuiCamera.apk -p Tools\APKTool\Frameworks
-java -jar Tools\APKTool\apktool.jar d --no-debug-info --output ..\ANXCamera_APK ROMS\REQUIRED_ROM\ROM\system\system\priv-app\MiuiCamera\MiuiCamera.apk -p Tools\APKTool\Frameworks
+java -jar %APKTOOL%\apktool.jar d --no-debug-info --output ..\ANXCamera_APK %SYSTEM%\system\priv-app\MiuiCamera\MiuiCamera.apk -p %APKTOOL%\Frameworks
 
 	:: Extract Classes [Trial and Error]
 	
 	:: miui.apk
-7z x ROMS\REQUIRED_ROM\ROM\system\system\app\miui\miui.apk -oTemp\miui *.dex
-7z x ROMS\REQUIRED_ROM\ROM\system\app\miui\miui.apk -oTemp\miui *.dex
+7z x %SYSTEM%\system\app\miui\miui.apk -oTemp\miui *.dex
 
 	:: miuisystem.apk
-7z x ROMS\REQUIRED_ROM\ROM\system\system\app\miuisystem\miuisystem.apk -oTemp\miuisystem *.dex
-7z x ROMS\REQUIRED_ROM\ROM\system\app\miuisystem\miuisystem.apk -oTemp\miuisystem *.dex
+7z x %SYSTEM%\system\app\miuisystem\miuisystem.apk -oTemp\miuisystem *.dex
 
 	:: framework.jar
-7z x ROMS\REQUIRED_ROM\ROM\system\system\framework\framework.jar -oTemp\framework *.dex
-7z x ROMS\REQUIRED_ROM\ROM\system\framework\framework.jar -oTemp\framework *.dex
+7z x %SYSTEM%\system\framework\framework.jar -oTemp\framework *.dex
 
 	:: gson.jar
-7z x ROMS\REQUIRED_ROM\ROM\system\system\framework\gson.jar -oTemp\gson *.dex
-7z x ROMS\REQUIRED_ROM\ROM\system\framework\gson.jar -oTemp\gson *.dex
+7z x %SYSTEM%\system\framework\gson.jar -oTemp\gson *.dex
 
 	:: Decompile Classes
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\miui Temp\miui\classes.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\miuisystem Temp\miuisystem\classes.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\framework Temp\framework\classes.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\framework Temp\framework\classes2.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\framework Temp\framework\classes3.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\framework Temp\framework\classes4.dex
-java -jar Tools\APKTool\baksmali.jar d -o ROMS\REQUIRED_ROM\Required_Classes\gson Temp\gson\classes.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\miui Temp\miui\classes.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\miuisystem Temp\miuisystem\classes.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\framework Temp\framework\classes.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\framework Temp\framework\classes2.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\framework Temp\framework\classes3.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\framework Temp\framework\classes4.dex
+java -jar %APKTOOL%\baksmali.jar d -o %CLASSES%\gson Temp\gson\classes.dex
 
 	:: Cleanup
 rmdir /Q /S Temp
